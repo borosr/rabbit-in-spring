f90a411650f020528aa6b96c72030ada5d743162
Boros Robert <robbie13.boros@gmail.com>
2018-10-06 21:00:56 +0200

    basic config with Receiver

---
+++ src/main/java/hu/borosr/rabbitmq/RabbitConfig.java
@@ -0,0 +1,50 @@
+package hu.borosr.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE_NAME = "rabbit_in_spring";
+    public static final String TOPIC_EXCHANGE_NAME = "rabbit_in_spring_exchange";
+
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("case.one.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+
+}
--- src/test/java/hu/borosr/rabbitmq/RabbitmqApplicationTests.java
+++ src/test/java/hu/borosr/rabbitmq/RabbitmqApplicationTests.java
@@ -2,15 +2,29 @@ package hu.borosr.rabbitmq;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RabbitmqApplicationTests {
 
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private Receiver receiver;
+
 	@Test
-	public void contextLoads() {
+	public void sendMessage() throws InterruptedException {
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(RabbitConfig.TOPIC_EXCHANGE_NAME,
+				"case.one.hello", "Hello from RabbitMQ!");
+		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
 	}
 
 }
